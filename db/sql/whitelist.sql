d7832a535c1df1444f08eed9b5176895e6ef4bc2
Viking Phoenix <vikingphoenixconsulting@gmail.com>
2022-01-13 23:08:58 -0700

    minor whitelist tweaks from testing

--- db/sql/whitelist.sql
+++ db/sql/whitelist.sql
@@ -5,7 +5,7 @@ CREATE TABLE "whitelist" (
     "eventId" INTEGER DEFAULT -1,
     "created_dtz" timestamptz NOT NULL DEFAULT now(), 
     "allowance_sigusd" NUMERIC(16,6) DEFAULT 0.0,
-    "remaining_sigusd" NUMERIC(16,6) DEFAULT 0.0,
+    "spent_sigusd" NUMERIC(16,6) DEFAULT 0.0,
     "isAvailable" SMALLINT DEFAULT 0,
     "lastAssemblerId" TEXT,
     "lastAssemblerStatus" TEXT
