ad64b9d0a0a641179e6660723d3bfb7a0e26a3b4
noob77777 <abhishekpal7@outlook.com>
2022-01-17 20:40:10 +0530

    :add: /api/assembler/return/{wallet}/{smartContract}

---
+++ backend/app/api/v1/routes/assembler.py
@@ -0,0 +1,28 @@
+import logging
+import requests
+
+from starlette.responses import JSONResponse
+from fastapi import APIRouter, status
+from config import Config, Network  # api specific config
+
+assembler_router = r = APIRouter()
+
+DEBUG = True
+levelname = (logging.WARN, logging.DEBUG)[DEBUG]
+logging.basicConfig(
+    format='{asctime}:{name:>8s}:{levelname:<8s}::{message}', style='{', levelname=levelname)
+
+CFG = Config[Network]
+headers = {'Content-Type': 'application/json'}
+
+
+@r.get("/return/{wallet}/{smartContract}", name="assembler:return")
+async def assemblerReturn(wallet: str, smartContract: str):
+    try:
+        res = requests.get(
+            f'{CFG.assembler}/return/{wallet}/{smartContract}')
+        return JSONResponse(status_code=res.status_code, content=res.json())
+    except:
+        logging.debug(
+            f'request failed for "wallet\": {wallet}, "smartContract": {smartContract}')
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=f'network failure could not connect to assembler')
--- backend/app/main.py
+++ backend/app/main.py
@@ -12,9 +12,10 @@ from api.v1.routes.projects import projects_router
 from api.v1.routes.vesting import vesting_router
 from api.v1.routes.whitelist import whitelist_router
 from api.v1.routes.events import events_router
+from api.v1.routes.assembler import assembler_router
 # from api.v1.routes.wallets import wallets_router
 # from api.v1.routes.tokens import tokens_router
-from api.v1.routes.purchase import purchase_router
+# from api.v1.routes.purchase import purchase_router
 
 from core import config
 # from app.db.session import SessionLocal
@@ -56,9 +57,11 @@ async def db_session_middleware(request: Request, call_next):
     return response
 """
 
+
 @app.get("/api/ping")
 async def ping():
     return {"hello": "world"}
+
 
 @app.get("/api/task")
 async def example_task():
@@ -66,16 +69,21 @@ async def example_task():
     return {"message": "success"}
 
 # Routers
-app.include_router(users_router, prefix="/api/users", tags=["users"], dependencies=[Depends(get_current_active_user)])
+app.include_router(users_router, prefix="/api/users",
+                   tags=["users"], dependencies=[Depends(get_current_active_user)])
 app.include_router(auth_router, prefix="/api/auth", tags=["auth"])
 app.include_router(asset_router, prefix="/api/asset", tags=["asset"])
-app.include_router(blockchain_router, prefix="/api/blockchain", tags=["blockchain"])
+app.include_router(blockchain_router,
+                   prefix="/api/blockchain", tags=["blockchain"])
+app.include_router(
+    assembler_router, prefix="/api/assembler", tags=["assembler"])
 app.include_router(projects_router, prefix="/api/projects", tags=["projects"])
 app.include_router(util_router, prefix="/api/util", tags=["util"])
 app.include_router(vesting_router, prefix="/api/vesting", tags=["vesting"])
-app.include_router(whitelist_router, prefix="/api/whitelist", tags=["whitelist"])
+app.include_router(
+    whitelist_router, prefix="/api/whitelist", tags=["whitelist"])
 app.include_router(events_router, prefix="/api/events", tags=["events"])
-app.include_router(purchase_router, prefix="/api/purchase", tags=["purchase"])
+# app.include_router(purchase_router, prefix="/api/purchase", tags=["purchase"])
 
 # MAIN
 if __name__ == "__main__":
