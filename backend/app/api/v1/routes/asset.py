b22f7b1478fcd0a96df8569fe966ea38201c6c4f
noob77777 <abhishekpal7@outlook.com>
2022-01-26 12:16:34 +0530

    :add: price history for ergo

--- backend/app/api/v1/routes/asset.py
+++ backend/app/api/v1/routes/asset.py
@@ -269,6 +269,7 @@ async def get_asset_historical_price(coin: str = "all", stepSize: int = 1, stepU
         for index, token in enumerate(tokens):
             if (token != coin and coin != "all"):
                 continue
+
             tokenData = {
                 "token": token,
                 "resolution": resolution,
@@ -285,6 +286,21 @@ async def get_asset_historical_price(coin: str = "all", stepSize: int = 1, stepU
                     "price": tokenUSD,
                 })
             result.append(tokenData)
+
+        # ergo
+        if coin in ("ergo", "all"):
+            tokenData = {
+                "token": "ergo",
+                "resolution": resolution,
+                "history": [],
+            }
+            for row in res:
+                tokenData["history"].append({
+                    "timestamp": row[0],
+                    "price": row[1],
+                })
+            result.append(tokenData)
+
         return result
     except Exception as e:
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=f'Error: {str(e)}')
